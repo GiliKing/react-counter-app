29ef785da58d2fd88f35193c98ce95baf20fbca9
giliking <chrisogili12@gmail>
2023-09-21 09:26:13 -0700

    updates

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from './features/counter/Counter'
+
+function App() {
+  return (
+    <div>
+      <h1>
+        My App
+        <Counter />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
